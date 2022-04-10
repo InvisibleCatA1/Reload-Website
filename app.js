486fda3a12f211fad5441dc84ce68d35f10e52e1
shiel <shieldscoen@gmail.com>
2022-04-10 17:45:52 -0600

    idk

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 
 menu.addEventListener('click', function() {
-    console.log("work")
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active')
 })
